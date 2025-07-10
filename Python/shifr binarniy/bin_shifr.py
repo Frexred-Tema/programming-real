e547b116bd5192210769766ac8564caf2d49ae35
Temirlan <omurbekovtmrln@gmail.com>
2025-07-10 16:57:12 +0300

    Я всё

---
+++ Python/shifr binarniy/bin_shifr.py
@@ -0,0 +1,35 @@
+word = str(input())
+word = word.upper()
+word = list(word.replace(' ', ''))
+alp = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+ 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+temp = []
+mw = []
+for i in range(len(word)):
+    for j in range(len(alp)):
+        if (len(temp) != 5):
+            if (word[i] == alp[j]):
+                temp.append(j)
+        elif (i == len(word)-1):
+            temp.append(j)
+            mw.append(temp)
+        else:
+            if (word[i] == alp[j]):
+                mw.append(temp)
+                temp = []
+                temp.append(j)
+
+print(mw)
+total = []
+for n in range(len(mw)):
+    t = mw[n][4] << 5
+    t = t | mw[n][3]
+    t = t << 5
+    t = t | mw[n][2]
+    t = t << 5
+    t = t | mw[n][1]
+    t = t << 5
+    t = t | mw[n][0]
+    total.append(t)
+
+print(total)
